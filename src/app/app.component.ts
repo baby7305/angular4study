ef677498c1f5e04fff3f1415eea008c0aa65f3f8
宋海贝 <songhaibei@shmingjiang.com.cn>
2017-11-16 15:16:37 +0800

    Hero对象

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   // templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`
+  template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>`
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero = 'baby7305';
+  hero: Hero = {
+    id: 1,
+    name: 'baby7305'
+  };
+}
+export class Hero {
+  id: number;
+  name: string;
 }
