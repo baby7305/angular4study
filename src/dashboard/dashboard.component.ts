890c9ee1128292c6c381dee0811ef2d322a97de0
songhaibei <songhaibei@shmingjiang.com.cn>
2017-11-20 10:55:03 +0800

    把英雄添加到仪表盘

--- src/dashboard/dashboard.component.ts
+++ src/dashboard/dashboard.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+import { Hero } from '../hero-detail/hero';
+import { HeroService } from '../hero-detail/hero.service';
 @Component({
     selector: 'dashboard',
     templateUrl: './dashboard.component.html',
     styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent { }
+export class DashboardComponent implements OnInit{ 
+    heroes: Hero[];
+    constructor(private heroService: HeroService) { }
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+    getHeroes(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes.slice(1, 5));
+    }
+}
